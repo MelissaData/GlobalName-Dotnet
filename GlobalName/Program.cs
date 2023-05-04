199758137dccab92c9fbd7e9cc3be415b31503f4
Melissa <ha@melissadata.com>
2023-05-04 08:32:59 -0700

    Commit .NET code for Melissa Global Name Cloud API

---
+++ GlobalName/Program.cs
@@ -0,0 +1,180 @@
+﻿using Newtonsoft.Json;
+
+namespace GlobalName
+{
+  static class Program
+  {
+    static void Main(string[] args)
+    {
+      string baseServiceUrl = @"https://globalname.melissadata.net/";
+      string serviceEndpoint = @"V3/WEB/GlobalName/doGlobalName"; //please see https://www.melissa.com/developer/global-name for more endpoints
+      string license = "";
+      string fullName = "";
+
+      ParseArguments(ref license, ref fullName, args);
+      CallAPI(baseServiceUrl, serviceEndpoint, license, fullName);
+    }
+
+    static void ParseArguments(ref string license, ref string fullName, string[] args)
+    {
+      for (int i = 0; i < args.Length; i++)
+      {
+        if (args[i].Equals("--license") || args[i].Equals("-l"))
+        {
+          if (args[i + 1] != null)
+          {
+            license = args[i + 1];
+          }
+        }
+        if (args[i].Equals("--fullname"))
+        {
+          if (args[i + 1] != null)
+          {
+            fullName = args[i + 1];
+          }
+        }
+      }
+    }
+
+    public static async Task GetContents(string baseServiceUrl, string requestQuery)
+    {
+      HttpClient client = new HttpClient();
+      client.BaseAddress = new Uri(baseServiceUrl);
+      HttpResponseMessage response = await client.GetAsync(requestQuery);
+
+      string text = await response.Content.ReadAsStringAsync();
+
+      var obj = JsonConvert.DeserializeObject(text);
+      var prettyResponse = JsonConvert.SerializeObject(obj, Newtonsoft.Json.Formatting.Indented);
+
+      // Print output
+      Console.WriteLine("\n================================ OUTPUT ================================\n");
+      
+      Console.WriteLine("API Call: ");
+      string APICall = Path.Combine(baseServiceUrl, requestQuery);
+      for (int i = 0; i < APICall.Length; i += 70)
+      {
+        try
+        {
+          Console.WriteLine(APICall.Substring(i, 70));
+        }
+        catch
+        {
+          Console.WriteLine(APICall.Substring(i, APICall.Length - i));
+        }
+      }
+
+      Console.WriteLine("\nAPI Response:");
+      Console.WriteLine(prettyResponse);
+    }
+
+    static void CallAPI(string baseServiceUrl, string serviceEndPoint, string license, string fullName)
+    {
+      Console.WriteLine("\n=============== WELCOME TO MELISSA GLOBAL NAME CLOUD API ===============\n");
+      
+      bool shouldContinueRunning = true;
+
+      while (shouldContinueRunning)
+      {
+        string inputFullName = "";
+
+        if (string.IsNullOrEmpty(fullName))
+        {
+          Console.WriteLine("\nFill in each value to see results");
+
+          Console.Write("FullName: ");
+          inputFullName = Console.ReadLine();
+        }
+        else
+        {
+          inputFullName = fullName;
+        }
+
+        while (string.IsNullOrEmpty(inputFullName))
+        {
+          Console.WriteLine("\nFill in missing required parameter");
+        
+          Console.Write("FullName: ");
+          inputFullName = Console.ReadLine();
+        }
+
+        Dictionary<string, string> inputs = new Dictionary<string, string>()
+        {
+          { "format", "json" },
+          { "full", inputFullName}
+        };
+
+        Console.WriteLine("\n================================ INPUTS ================================\n");
+        Console.WriteLine($"\t   Base Service Url: {baseServiceUrl}");
+        Console.WriteLine($"\t  Service End Point: {serviceEndPoint}");
+        Console.WriteLine($"\t           FullName: {inputFullName}");
+
+        // Create Service Call
+        // Set the License String in the Request
+        string RESTRequest = "";
+
+        RESTRequest += @"&id=" + Uri.EscapeDataString(license);
+
+        // Set the Input Parameters
+        foreach (KeyValuePair<string, string> kvp in inputs)
+          RESTRequest += @"&" + kvp.Key + "=" + Uri.EscapeDataString(kvp.Value);
+
+        // Build the final REST String Query
+        RESTRequest = serviceEndPoint + @"?" + RESTRequest;
+
+        // Submit to the Web Service. 
+        bool success = false;
+        int retryCounter = 0;
+
+        do
+        {
+          try //retry just in case of network failure
+          {
+            GetContents(baseServiceUrl, $"{RESTRequest}").Wait();
+            Console.WriteLine();
+            success = true;
+          }
+          catch (Exception ex)
+          {
+            retryCounter++;
+            Console.WriteLine(ex.ToString());
+            return;
+          }
+        } while ((success != true) && (retryCounter < 5));
+
+        bool isValid = false;
+        if (!string.IsNullOrEmpty(fullName))
+        {
+          isValid = true;
+          shouldContinueRunning = false;
+        }
+
+        while (!isValid)
+        {
+          Console.WriteLine("\nTest another record? (Y/N)");
+          string testAnotherResponse = Console.ReadLine();
+
+          if (!string.IsNullOrEmpty(testAnotherResponse))
+          {
+            testAnotherResponse = testAnotherResponse.ToLower();
+            if (testAnotherResponse == "y")
+            {
+              isValid = true;
+            }
+            else if (testAnotherResponse == "n")
+            {
+              isValid = true;
+              shouldContinueRunning = false;
+            }
+            else
+            {
+              Console.Write("Invalid Response, please respond 'Y' or 'N'");
+            }
+          }
+        }
+      }
+      
+      Console.WriteLine("\n================= THANK YOU FOR USING MELISSA CLOUD API ================\n");
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,101 @@
+# Melissa - Global Name Cloud API
+
+## Purpose
+This code showcases the Melissa Global Name Cloud API using C#.
+
+Please feel free to copy or embed this code to your own project. Happy coding!
+
+For the latest Melissa Global Name release notes, please visit: https://releasenotes.melissa.com/cloud-api/global-name/
+
+For further documentation, please visit: https://www.melissa.com/quickstart-guides/global-name
+
+The console will ask the user for:
+
+- FullName
+
+And return information of the name such as:
+
+- Results
+- Company
+- NamePrefix
+- NameFirst
+- NameMiddle
+- NameLast
+- NameSuffix
+- NameNickname
+- NameProfTitle
+- Gender
+- Salutation
+
+## Tested Environments
+- Windows 64-bit .NET 7.0
+- Global Name Cloud API Version 7.2.1.4100
+
+## Getting Started
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Install the Dotnet Core SDK
+Before starting, make sure that the .NET 7.0 SDK has been correctly installed on your machine (If you have Visual Studio installed, you most likely have it already). If you are unsure, you can check by opening a command prompt window and typing the following:
+
+`dotnet --list-sdks`
+
+If the .NET 7.0 SDK is already installed, you should see it in the following list:
+
+![alt text](/screenshots/dotnet_output.png)
+
+As long as the above list contains version `7.0.xxx` (underlined in red), then you can skip to the next step. If your list does not contain version 7.0, or you get any kind of error message, then you will need to download and install the .NET 7.0 SDK from the Microsoft website.
+
+To download, follow this link: https://dotnet.microsoft.com/en-us/download/dotnet
+
+Select `.NET 7.0` and you will be navigated to the download page.
+
+Click and download the `x64` SDK installer for your operating system.
+
+(IMPORTANT: Make sure you download the SDK, NOT the runtime. the SDK contains both the runtime as well as the tools needed to build the project.)
+
+![alt text](/screenshots/net7.png)
+
+Once clicked, your web browser will begin downloading an installer for the SDK. Run the installer and follow all of the prompts to complete the installation (your computer may ask you to restart before you can continue). Once all of that is done, you should be able to verify that the SDK is installed with the `dotnet --list-sdks` command.
+
+### Download this project
+```
+$ git clone https://github.com/MelissaData/GlobalName-Dotnet.git
+$ cd GlobalName-Dotnet
+```
+
+## Run Powershell Script
+Parameters:
+- -fullname: an input name
+
+  This is convenient when you want to get results for a specific name in one run instead of testing multiple records in interactive mode.  
+
+- -license (optional): a license string to test the Cloud API
+
+There are two modes:
+
+- Interactive 
+
+	The script will prompt the user for input(s), then use the provided input(s) to call the Cloud API. For example:
+	```
+	$ .\GlobalName.ps1
+	```
+
+- Command Line 
+
+	You can pass a full name and a license string into `-fullname` and `-license` parameters respectively to test the Cloud API. For example: 
+	```
+    $ .\GlobalName.ps1 -fullname "Raymond Melissa" 
+    $ .\GlobalName.ps1 -fullname "Raymond Melissa" -license "<your_license_string>"
+    ```
+
+This is the expected output from a successful setup for interactive mode:
+
+![alt text](/screenshots/output.png)
+
+## Result Codes
+For details about the result codes please refer to https://wiki.melissadata.com/index.php?title=Result_Code_Details#Global_Name 
+
+## Contact Us
+For free technical support, please call us at 800-MELISSA ext. 4 (800-635-4772 ext. 4) or email us at tech@melissa.com.
+
+To purchase this product, contact the Melissa sales department at 800-MELISSA ext. 3 (800-635-4772 ext. 3).
